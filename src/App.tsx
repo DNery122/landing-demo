8c1945874a8b8da7909588a25bc4c7adfbf2208c
Daniel Neri Herrera <daniel.nerih@sfa.michoacan.gob.mx>
2026-08-25 16:09:06 -0600

    Fix FormEvent type import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { FormEvent, useState } from "react";
+import { useState } from 'react';
+import type { FormEvent } from 'react';
 
 function App() {
   const [sending, setSending] = useState(false);
